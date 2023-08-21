330f96847546cecd565ae8698c748ada0aa50536
Cameron St.Rose <ckstrose@gmail.com>
2023-08-21 17:35:33 -0400

    removed locators from .py file and put in HW3 Step 1 Locators file

--- features/steps/amazon_signin_&_cart.py
+++ features/steps/amazon_signin_&_cart.py
@@ -6,22 +6,6 @@ SEARCH_INPUT = (By.NAME, 'q')
 SEARCH_SUBMIT = (By.NAME, 'btnK')
 
 
-"""
-Locators For Amazon Create Account Page
-
-Amazon Logo: driver.find_element(By. CSS_SELECTOR, ".a-icon.a-icon-logo")
-Create Account: driver.find_element(By. XPATH, "//h1[@class='a-spacing-small']")
-Your Name Field: driver.find_element(By. CSS_SELECTOR, "#ap_customer_name")
-Email Field: driver.find_element(By. CSS_SELECTOR, "#ap_email")
-Password Field: driver.find_element(By. CSS_SELECTOR, "#ap_password")
-Re_enter Password Field: driver.find_element(By. CSS_SELECTOR, "#ap_password_check")
-Create Amazon Account/Continue Button: driver.find_element(By. CSS_SELECTOR, "#continue")
-Conditions of Use: driver.find_element(By. CSS_SELECTOR, "[href*='condition_of_use?']")
-Privacy Notice: driver.find_element(By. CSS_SELECTOR, "[href*='register_notification_privacy_notice?']")
-Sign In Button: driver.find_element(By. CSS_SELECTOR, "[href*='/ap/signin?']")
-"""
-
-
 @given('Open Amazon page')
 def open_amazon(context):
     context.driver.get('https://www.amazon.com/')
